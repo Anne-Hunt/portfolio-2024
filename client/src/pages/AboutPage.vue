273254b6793a5fdcaf78bfca8145929f3a477e39
Anne Hunt <acathunt@gmail.com>
2024-06-27 19:40:32 -0600

    working on layout

--- client/src/pages/AboutPage.vue
+++ client/src/pages/AboutPage.vue
@@ -3,91 +3,14 @@
 </script>
 
 <template>
-    <div class="keeb p-5">
-        <Keyboard/>
-    </div>
-  <div class="about">
-    <h1>This is the about page</h1>
-  </div>
-  <section class="row">
-    <div class="col-9 p-3">
-                <div class="education ps-2">
-                    <h2 class="border-bottom border-5">Education</h2>
-                    <div class="school">
-                        <h4 class="institution">Boise Codeworks</h4>
-                        <span class="city">Boise, ID</span><span class="time">(2024)</span><br>
-                        <span class="degree">Immersive Full Stack</span>
-                        <p class="learned">HTML, CSS, Javascript, Python, C#</p>
-                    </div>
-                    <div class="school">
-                        <h4 class="institution">University of Wisconsin-Milwaukee</h4>
-                        <span class="city">Milwaukee, WI</span><span class="time">(2010-2011)</span><br>
-                        <span class="degree">PhD (unfinished) Composition and Rhetoric</span>
-                        <p class="learned">Teaching, philosophy, history of education and pedagogy</p>
-                    </div>
-                    <div class="school">
-                        <h4 class="institution">University of Wyoming</h4>
-                        <span class="city">Laramie, WY</span>
-                        <span class="time">(2008-2010)</span><br>
-                        <span class="degree">Master of Arts in Theory and Pedagogy of Composition and Rhetoric</span>
-                        <p class="learned">Digital media use in pedagogy, creation of digital media, analysis, writing,
-                            Adobe Creative Suite</p>
-                    </div>
-                    <div class="school">
-                        <h4 class="institution">Black Hills State University</h4>
-                        <span class="city">Spearfish, SD</span>
-                        <span class="time">(2004-2008)</span><br>
-                        <span class="degree">Bachelor of Arts in English</span>
-                        <span class="minors">Writing, Library Science, Spanish</span>
-                        <p class="learned">Writing, pedagogy, content and marketing media, analysis</p>
-                    </div>
-                </div>
+<section class="row p-3 bg-light">
+      <h1>About Anne Hunt</h1>
+      <div class="col">
 
-                <div class="workhistory">
-                    <h2 class="border-bottom border-5">Employment</h2>
-                    <div class="job">
-                        <h4 class="jobtitle">Owner</h4>
-                        <span class="company">Quick Wit Marketing</span>
-                        <span class="time">(2015-2019)</span>
-                        <p class="responsibilities">Maintaining online marketing presences for local businesses,
-                            acquiring clients, project management, event coordination</p>
-                        <p class="skillsreq">Content creation, basic web development</p>
-                    </div>
-                    <div class="job">
-                        <h4 class="jobtitle">Marketing Manager</h4>
-                        <span class="company">Atlas Resell Management</span>
-                        <span class="time">(2011-2015)</span>
-                        <p class="responsibilities">Data management, planning and deploying marketing strategies</p>
-                        <p class="skillsreq">Excel, MySQL, content writing, project management, SEO</p>
-                    </div>
-                    <div class="job">
-                        <h4 class="jobtitle">Communications Director</h4>
-                        <span class="company">Milwaukee Graduate Assistant Association</span>
-                        <span class="time">(2010-2011)</span>
-                        <p class="responsibilities">Communication with members regarding contract negotiations, event
-                            planning and coordination</p>
-                        <p class="skillsreq">Wordpress, social media, organization</p>
-                    </div>
-                    <div class="job">
-                        <h4 class="jobtitle">Instructor</h4>
-                        <span class="company">University of Wisconsin-Milwaukee</span>
-                        <span class="time">(2010-2011)</span><br>
-                        <span class="company">University of Wyoming</span>
-                        <span class="time">(2008-2010)</span>
-                        <p class="responsibilities">Teaching required English composition courses to students from a
-                            diverse range of backgrounds</p>
-                        <p class="skillsreq">Understanding of pedagogy, digital media use in classroom</p>
-                    </div>
-                    <div class="job">
-                        <h4 class="jobtitle">Marketing and Media Content Writer</h4>
-                        <span class="company">Black Hills State University</span>
-                        <span class="time">(2006-2008)</span>
-                        <p class="responsibilities">Writing press releases, marketing content, and reporting on campus
-                            events</p>
-                        <p class="skillsreq">HTML, SEO, writing</p>
-                    </div>
-                </div>
-            </div>
+      </div>
+    <div class="col p-3">
+        <img src="../assets/img/dotsheadshot.png" alt="">
+    </div>
   </section>
 </template>
 
