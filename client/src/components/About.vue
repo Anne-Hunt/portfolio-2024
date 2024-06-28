d91b840d4e9e027122aede4de75fb3e92034e0d1
Anne Hunt <acathunt@gmail.com>
2024-06-27 22:14:01 -0600

    resume updates

---
+++ client/src/components/About.vue
@@ -0,0 +1,90 @@
+<script setup>
+
+</script>
+
+
+<template>
+      <section class="row p-0 justify-content-center m-0">
+       <div class="row justify-content-center">
+        <h2>About Me</h2>
+        <div class="col-12 order-md-1 order-2">
+          <p>
+            If I have learned anything in my life, it's how to learn. I'm particularly gifted especially when it comes to language, whether coding or human communication, as I proved in my coding bootcamp and when I almost completed an entire bachelor's in Spanish in a year. I love solving problems and creating simple, elegant solutions that incorporate the future plans for applications and aren't just a bandaid on the issue at hand. Creating is what I do - whether I'm knitting a sweater or making an app.
+          </p>
+          <div class="row justify-content-center">
+            <div class="col-md-4 col-12">
+              <h5>Coding</h5>
+              <ul>
+                <li>HTML/CSS</li>
+                <li>Javascript</li>
+                <li>Boostrap 5</li>
+                <li>Vue.js 3</li>
+                <li>C#</li>
+                <li>.NET CORE</li>
+              </ul>
+            </div>
+            <div class="col-md-4 col-12">
+              <h5>Working</h5>
+              <ul>
+                <li>Scrum/Agile</li>
+                <li>Problem Solving</li>
+                <li>Process Improvement</li>
+                <li>Leadership</li>
+                <li>Communication</li>
+                <li>Organization</li>
+              </ul>
+            </div>
+            <div class="col-md-4 col-12">
+              <h5>Creative</h5>
+              <ul>
+                <li>Knitting</li>
+                <li>Book binding</li>
+                <li>Baking</li>
+                <li>Car Repair</li>
+                <li>Woodworking</li>
+                <li>Sewing</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+       </div>
+      </section>
+</template>
+
+
+<style scoped lang="scss">
+h1, h2, h3, h4, h5{
+  font-family: "Kanit", sans-serif;
+  font-weight: 700;
+  font-style: normal;
+}
+
+.bg{
+  background-color: black;
+  background-image: linear-gradient( 76.9deg,  rgba(255,90,90,1) 27.2%, rgba(130,5,255,1) 79.9% );
+  height: auto;
+}
+
+.bdy{
+  height: auto;
+  mix-blend-mode: screen;
+  background-color: var(--bs-page);
+  color: black;
+  font-size: 10vw; /* Responsive font size */
+  font-weight: bold;
+  text-shadow: black;
+  font-family: Asap, Verdana, Geneva, Tahoma, sans-serif;
+}
+
+.bg-page{
+  background-color: var(--bs-page);
+}
+
+.screen{
+  mix-blend-mode: screen;
+  background-color: var(--bs-page);
+  color: black;
+  font-weight: bold;
+  text-shadow: black;
+}
+</style>
--- client/src/pages/AboutPage.vue
+++ client/src/pages/AboutPage.vue
@@ -3,20 +3,55 @@
 </script>
 
 <template>
-<section class="row p-3 bg-light">
-    <div class="col">
-          <h1>About Anne Hunt</h1>
+<section class="row fill px-4 m-0 py-0 bg-page justify-content-center align-items-top">
+  <div class="row align-items-center justify-content-evenly">
 
-      </div>
-    <div class="col p-3">
-        <img src="../assets/img/dotsheadshot.png" alt="">
+    <div class="col px-0">
+      <About/>
     </div>
+    <div class="col p-0">
+      <div class="row">
+        <div class="col-12 p-0 m-0 d-flex justify-content-center">
+          <img src="../assets/img/dotsheadshot.png" alt="Face of Anne Hunt">
+        </div>
+        <div class="col-12 text-center p-0 m-0">
+          <h3>A face you can trust!</h3>
+        </div>
+      </div>
+    </div>
+  </div>
   </section>
 </template>
 
 <style scoped lang="scss">
-.keeb{
-    width: 450px;
-    height: 150px;
+h1, h2, h3, h4, h5{
+  font-family: "Kanit", sans-serif;
+  font-weight: 700;
+  font-style: normal;
+}
+.bg-page{
+  background-color: var(--bs-page);
+}
+.bg{
+  background-color: black;
+  background-image: linear-gradient( 76.9deg,  rgba(255,90,90,1) 27.2%, rgba(130,5,255,1) 79.9% );
+  height: auto;
+}
+
+.screen{
+  mix-blend-mode: screen;
+  background-color: var(--bs-page);
+  color: black;
+  font-weight: bold;
+  text-shadow: black;
+}
+
+img{
+    max-height: 80dvh;
+}
+
+.fill{
+  height: 100dvh;
+
 }
 </style>
